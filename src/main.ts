e268d7fb67c4b24d3bf9b29410848f5558fecbd0
Oleksandr <lantainzp@gmail.com>
2023-07-15 22:28:24 +0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(`Starting on ${process.env.PORT}:${process.env.HOST}...`)
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
